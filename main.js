db401ffae6cdca2d16acbb610a5f33a9d2bdf651
Tharindu Yalegama <87438005+tharinduyalegamalucky@users.noreply.github.com>
2022-02-14 07:25:40 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    if(nameInput.value === '' || emailInput.value === '') {
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+
+        setTimeout(() => msg.remove(), 3000);
+    }else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+        userList.appendChild(li);
+
+        //clear fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
